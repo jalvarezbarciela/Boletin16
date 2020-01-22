7fca2892dcd3afce464d89645c243f8669cebeb5
jalvarezbarciela <>
2020-01-22 10:20:45 +0100

    Boletin 16

---
+++ src/bol16/Bol16.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol16;
+
+/**
+ *
+ * @author ceque
+ */
+public class Bol16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/bol16/Persoa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol16;
+
+/**
+ *
+ * @author ceque
+ */
+public class Persoa implements IPodeCantar{
+
+    @Override
+    public void Cantar() {
+        System.out.println("DO RE MI FA SOL LA SI DO");
+    }
+    
+}
